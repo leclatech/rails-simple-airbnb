2c820f6d811278d972426d5577887e5d11b8b365
Leone Claser <leoneclaser@gmail.com>
2023-11-15 22:24:08 +0100

    airbnb almost

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "flats", to: "flats#index"
+
+  get "flats/new", to: "flats#new"
+
+  get "flats/:id", to: "flats#show", as: :flat
+
+  post "flats", to: "flats#create"
+
+  get "flats/:id/edit", to: "flats#edit", as: :edit_flat
+  patch "flats/:id", to: "flats#update"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Dark but Spacious Flat in Anderlecht',
+  address: 'Rue Rossini 38',
+  description: 'A lovely cemetary feel for this spacious flat. Two double grave, open plan dying area, large kitchen and a beautiful crematory',
+  price_per_night: 10,
+  number_of_guests: 1
+)
+Flat.create!(
+  name: 'Dark but Spacious Flat in Ixelles',
+  address: 'Rue Rossini 39',
+  description: 'A lovely cemetary feel for this spacious flat. Two double grave, open plan dying area, large kitchen and a beautiful crematory',
+  price_per_night: 10,
+  number_of_guests: 1
+)
+Flat.create!(
+  name: 'Dark but Spacious Flat in LA',
+  address: 'Rue Rossini 37',
+  description: 'A lovely cemetary feel for this spacious flat. Two double grave, open plan dying area, large kitchen and a beautiful crematory',
+  price_per_night: 10,
+  number_of_guests: 1
+)
+Flat.create!(
+  name: 'Dark but Spacious Flat in Budrio',
+  address: 'Rue Rossini 36',
+  description: 'A lovely cemetary feel for this spacious flat. Two double grave, open plan dying area, large kitchen and a beautiful crematory',
+  price_per_night: 10,
+  number_of_guests: 1
+)
